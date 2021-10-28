dd323d4a125a19f6d9f4a061ff93257b0edfbf8c
kennedybryant <kennedy_marisa96@yahoo.com>
2021-10-28 12:56:30 -0400

    work in progress

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "plugin:react/recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 13
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
